6b71b89b14868370987ed636f16f411797381e33
Tamas Molnar <gtamas.molnar3@gmail.com>
2020-04-04 13:35:16 +0200

    MC

--- src/stubs/resources/js/bootstrap.js
+++
@@ -1,9 +0,0 @@
-try {
-    window.$ = window.jQuery = require('jquery');
-
-    require('stubs/resources/js/bootstrap');
-} catch (e) {}
-
-window.axios = require('axios');
-
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
